442861efe770695944222a2ddfb13f92669dca8e
allxvr <all4vrnn@gmail.com>
2023-02-02 20:49:18 +0300

    add some changes to task29

--- CshSem4HW/task29/Program.cs
+++ CshSem4HW/task29/Program.cs
@@ -4,16 +4,24 @@
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19] 
 6, 1, 33 -> [6, 1, 33] 
 */
+Console.Clear();
+int min = ConsRead("Введите минимальное число массива: ");
+int max = ConsRead("Введите минимальное число массива: ");
+int[] array = randArr(8, min, max);
+Console.WriteLine($"{String.Join(", ", array)} -> [ {String.Join(", ", array)} ] ");
 
-int lenArr = ConsRead("Введите длину массива: ");
-
-int[] randArr = new int[lenArr];
-for (int i = 0; i < randArr.Length; i++)
+// Функция формирования массива из N элементов, с мин и макс числом значения в ячейке
+int[] randArr(int size, int minNum, int maxNum)
 {
-    randArr[i] = new Random().Next(1,9);
-    Console.Write(randArr[i] + " ");
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(minNum, maxNum + 1);
+        // Console.Write(arr[i] + " ");
+    }
+    return arr;
+        Console.Write($" -> ");
 }
-
 
 // Метод ввода в консоль
 int ConsRead(string mes)
@@ -21,6 +29,9 @@ int ConsRead(string mes)
     Console.Write(mes);
     return Convert.ToInt32(Console.ReadLine());
 }
+
+
+
 
 
 
